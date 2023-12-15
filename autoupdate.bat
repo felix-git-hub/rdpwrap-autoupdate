782caa9fd99b7c0f46fe7ec50e3e40444a7dbc3c
felix-git-hub <70873830+felix-git-hub@users.noreply.github.com>
2023-12-15 11:31:50 +0800

    Update autoupdate.bat

--- autoupdate.bat
+++ autoupdate.bat
@@ -1,70 +1,41 @@
-<!-- : Begin batch script
 @echo off
 setLocal EnableExtensions
 setlocal EnableDelayedExpansion
+:: -----------------------------------------
+:: Location of new/updated rdpwrap.ini files
+:: -----------------------------------------
 
-REM -------------------------------------------------------------------
-REM
-REM                        autoupdate.bat
-REM
-REM Automatic RDP Wrapper installer and updater // asmtron (07-09-2019)
-REM ADD new dowload URL // (01-31-2021)
-REM -------------------------------------------------------------------
-REM Options:
-REM   -log        = redirect display output to the file autoupdate.log
-REM   -taskadd    = add autorun of autoupdate.bat on startup in schedule task
-REM   -taskremove = remove autorun of autoupdate.bat on startup in schedule task
-REM
-REM Info:
-REM   The autoupdater first use and check the official rdpwrap.ini.
-REM   If a new termsrv.dll is not supported in the offical rdpwrap.ini,
-REM   autoupdater first tries the asmtron rdpwrap.ini (disassembled and
-REM   tested by asmtron). The autoupdater will also use rdpwrap.ini files
-REM   of other contributors like the one of "saurav-biswas".
-REM   Extra rdpwrap.ini sources can also be defined...
-REM
-REM { Special thak to binarymaster, saurav-biswas and all other contributors }
+:: Define array 定义数组
+set rdpwrap_ini_update_github[0]="https://raw.githubusercontent.com/asmtron/rdpwrap/master/res/rdpwrap.ini"
+set rdpwrap_ini_update_github[1]="https://raw.githubusercontent.com/sebaxakerhtc/rdpwrap.ini/master/rdpwrap.ini"
+set rdpwrap_ini_update_github[2]="https://raw.githubusercontent.com/affinityv/INI-RDPWRAP/master/rdpwrap.ini"
+set rdpwrap_ini_update_github[3]="https://raw.githubusercontent.com/DrDrrae/rdpwrap/master/res/rdpwrap.ini"
+set rdpwrap_ini_update_github[4]="https://raw.githubusercontent.com/saurav-biswas/rdpwrap-1/master/res/rdpwrap.ini"
+:: set rdpwrap_ini_update_github_6="https://raw.githubusercontent.com/....Extra.6...."
+:: set rdpwrap_ini_update_github_7="https://raw.githubusercontent.com/....Extra.7...."
 
-REM -----------------------------------------
-REM Location of new/updated rdpwrap.ini files
-REM -----------------------------------------
-set rdpwrap_ini_update_github_1="https://raw.githubusercontent.com/asmtron/rdpwrap/master/res/rdpwrap.ini"
-set rdpwrap_ini_update_github_2="https://raw.githubusercontent.com/affinityv/INI-RDPWRAP/master/rdpwrap.ini"
-set rdpwrap_ini_update_github_3="https://raw.githubusercontent.com/DrDrrae/rdpwrap/master/res/rdpwrap.ini"
-set rdpwrap_ini_update_github_4="https://raw.githubusercontent.com/saurav-biswas/rdpwrap-1/master/res/rdpwrap.ini"
-REM set rdpwrap_ini_update_github_3="https://raw.githubusercontent.com/....Extra.3...."
-REM set rdpwrap_ini_update_github_4="https://raw.githubusercontent.com/....Extra.4...."
+:: Get the length of the Array
+set length=0
+for /F "tokens=* delims=" %%i in ('set rdpwrap_ini_update_github[') do set /A length+=1
+echo "We will get ini from" %length% "urls"
 
 set autoupdate_bat="%~dp0autoupdate.bat"
 set autoupdate_log="%~dp0autoupdate.log"
 set RDPWInst_exe="%~dp0RDPWInst.exe"
-set rdpwrap_ini="%~dp0rdpwrap.ini"
-set rdpwrap_ini_check=%rdpwrap_ini%
+set rdpwrap_dll="%~dp0rdpwrap.dll"
+set rdpwrap_ini="C:\Program Files\RDP Wrapper\rdpwrap.ini"
 set rdpwrap_new_ini="%~dp0rdpwrap_new.ini"
-set github_location=1
+set rdpwrap_ini_check=%rdpwrap_new_ini%
+set rdpfolder="C:\Program Files\RDP Wrapper\"
 
-echo ___________________________________________
-echo Automatic RDP Wrapper installer and updater
-echo.
-echo ^<check if the RDP Wrapper is up-to-date and working^>
-echo.
-REM check if admin
-fsutil dirty query %systemdrive% >nul
-if not %errorlevel% == 0 goto :not_admin
-REM check for arguments
+set github_location=1
+set retry_network_check=0
+set proxyserver="localhost:62090"
+
+:: check for arguments
 if /i "%~1"=="-log" (
     echo %autoupdate_bat% output from %date% at %time% > %autoupdate_log%
     call %autoupdate_bat% >> %autoupdate_log%
-    goto :finish
-)
-if /i "%~1"=="-taskadd" (
-    echo [+] add autorun of %autoupdate_bat% on startup in the schedule task.
-    schtasks /create /f /sc ONSTART /tn "RDP Wrapper Autoupdate" /tr "cmd.exe /C \"%~dp0autoupdate.bat\" -log" /ru SYSTEM /delay 0000:10
-    goto :finish
-)
-if /i "%~1"=="-taskremove" (
-    echo [-] remove autorun of %autoupdate_bat% on startup in the schedule task^^!
-    schtasks /delete /f /tn "RDP Wrapper Autoupdate"
     goto :finish
 )
 if /i not "%~1"=="" (
@@ -75,269 +46,213 @@ if /i not "%~1"=="" (
     echo     -taskremove  =  remove autorun of autoupdate.bat on startup in the schedule task
     goto :finish
 )
-REM check if file "RDPWInst.exe" exist
-if not exist %RDPWInst_exe% goto :error_install
-goto :start_check
+:: check if admin
+::fsutil dirty query %systemdrive% >nul
+::if not %errorlevel% == 0 goto :finish
 
-:not_admin
-echo [-] This script must be run as administrator to work properly^^!
-echo     ^<Please use 'right click' on this batch file and select "Run As Administrator"^>
-echo.
-goto :finish
-:error_install
-echo [-] RDP Wrapper installer executable (RDPWInst.exe) not found^^!
-echo Please extract all files from the downloaded RDP Wrapper package or check your Antivirus.
-echo.
-goto :finish
 
-:start_check
-set rdpwrap_installed="0"
-REM ----------------------------------
-REM 1) check if TermService is running
-REM ----------------------------------
-sc queryex "TermService"|find "STATE"|find /v "RUNNING" >nul&&(
-    echo [-] TermService NOT running^^!
-    call :install
-)||(
-    echo [+] TermService running.
-)
-REM ------------------------------------------
-REM 2) check if listener session rdp-tcp exist
-REM ------------------------------------------
-set rdp_tcp_session=""
-for /f "tokens=1-2* usebackq" %%a in (
-    `query session rdp-tcp`
-) do (
-    set rdp_tcp_session=%%a
-    set rdp_tcp_session_id=%%b
-)
-if %rdp_tcp_session%=="" (
-    echo [-] Listener session rdp-tcp NOT found^^!
-    call :install
-) else (
-    echo [+] Found listener session: %rdp_tcp_session% ^(ID: %rdp_tcp_session_id%^).
-)
-REM -----------------------------------------
-REM 3) check if rdpwrap.dll exist in registry
-REM -----------------------------------------
-reg query "HKLM\SYSTEM\CurrentControlSet\Services\TermService\Parameters" /f "rdpwrap.dll" >nul&&(
-    echo [+] Found windows registry entry for "rdpwrap.dll".
-)||(
-    echo [-] NOT found windows registry entry for "rdpwrap.dll"^^!
-    if %rdpwrap_installed%=="0" (
-        call :install
-    )
-)
-REM ------------------------------
-REM 4) check if rdpwrap.ini exists
-REM ------------------------------
+
+:: ----------------------------------------------------
+:: 1) get file version of %windir%\System32\termsrv.dll 这段代码用于获取 Windows 中 termsrv.dll 文件的版本号，并将其存储在变量 %termsrv_dll_ver% 中。
+:: ----------------------------------------------------
+
+
+for /f "usebackq delims=" %%a in (
+`powershell -c "Split-Path -Leaf (Get-Item %windir%\System32\termsrv.dll).VersionInfo.ProductVersionRaw"`
+) do set "termsrv_dll_ver=%%a"
+
+echo [+] Installed "termsrv.dll" version: %termsrv_dll_ver%.
+
+::
+:: --------------------------------------------------------------------
+:: Check Current RDP Wrapper is up-to-date or not
+:: -------------------------------------------------------------------
+
 if exist %rdpwrap_ini% (
-    echo [+] Found file: %rdpwrap_ini%.
-) else (
-    echo [-] File NOT found: %rdpwrap_ini%^^!
-    if %rdpwrap_installed%=="0" (
-        call :install
+    echo [*] Start searching [%termsrv_dll_ver%] version entry in file %rdpwrap_ini%...
+    findstr /c:"[%termsrv_dll_ver%]" %rdpwrap_ini% >nul
+    if not errorlevel 1 (
+            echo %rdpwrap_ini%
+            echo [+] Found "termsrv.dll" version entry [%termsrv_dll_ver%] in file %rdpwrap_ini%.
+            echo [*] RDP Wrapper seems to be up-to-date and working...
+            call :finish
+    ) else (
+            echo [-] NOT found "termsrv.dll" version entry [%termsrv_dll_ver%] in file %rdpwrap_ini%^^!
+        
     )
-)
-REM ---------------------------------------------------------------
-REM 5) get file version of %windir%\System32\termsrv.dll
-REM ---------------------------------------------------------------
-for /f "tokens=* usebackq" %%a in (
-    `cscript //nologo "%~f0?.wsf" //job:fileVersion "%windir%\System32\termsrv.dll"`
-) do (
-    set termsrv_dll_ver=%%a
-)
-if "%termsrv_dll_ver%"=="" (
-    echo [x] Error on getting the file version of "%windir%\System32\termsrv.dll"^^!
-    goto :finish
+
+
 ) else (
-    echo [+] Installed "termsrv.dll" version: %termsrv_dll_ver%.
+    :: 如果文件 %rdpwrap_ini_check% 不存在，则给出错误提示，并跳转至 :finish 结束程序。
+    echo [-] File NOT found: %rdpwrap_ini%.
+    call :check_update
+
 )
-REM ----------------------------------------------------------------------------------------
-REM 6) check if installed fileversion is different to the last saved fileversion in registry
-REM ----------------------------------------------------------------------------------------
-echo [*] Read last "termsrv.dll" version from the windows registry...
-for /f "tokens=2* usebackq" %%a in (
-    `reg query "HKEY_LOCAL_MACHINE\SOFTWARE\RDP-Wrapper\Autoupdate" /v "termsrv.dll" 2^>nul`
-) do (
-    set last_termsrv_dll_ver=%%b
+
+:: --------------------------------------------------------------------
+:: 2) check if installed termsrv.dll version exists in rdpwrap.ini 这段代码的作用是检查 RDP Wrapper 是否需要更新，并进行相应的操作。具体细节如下：
+:: --------------------------------------------------------------------
+for /l %%i in (0,1,%length%) do (
+    set rdpwrap_ini_url=!rdpwrap_ini_update_github[%%i]!
+::    call :update
+    call :check_update
+
 )
-if "%last_termsrv_dll_ver%"=="%termsrv_dll_ver%" (
-    echo [+] Current "termsrv.dll v.%termsrv_dll_ver%" same as last "termsrv.dll v.%last_termsrv_dll_ver%".
-) else (
-    echo [-] Current "termsrv.dll v.%termsrv_dll_ver%" different from last "termsrv.dll v.%last_termsrv_dll_ver%"^^!
-    echo [*] Update current "termsrv.dll" version to the windows registry...
-    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\RDP-Wrapper\Autoupdate" /v "termsrv.dll" /t REG_SZ /d "%termsrv_dll_ver%" /f
-    if %rdpwrap_installed%=="0" (
-        call :install
-    )
-)
-REM ---------------------------------------------------------------
-REM 7) check if installed termsrv.dll version exists in rdpwrap.ini
-REM ---------------------------------------------------------------
+
+::
+:: --------------------------------------------------------------------
+:: update ini by git
+:: -------------------------------------------------------------------
+
 :check_update
 if exist %rdpwrap_ini_check% (
     echo [*] Start searching [%termsrv_dll_ver%] version entry in file %rdpwrap_ini_check%...
-    findstr /c:"[%termsrv_dll_ver%]" %rdpwrap_ini_check% >nul&&(
-        echo [+] Found "termsrv.dll" version entry [%termsrv_dll_ver%] in file %rdpwrap_ini_check%.
-        echo [*] RDP Wrapper seems to be up-to-date and working...
-    )||(
-        echo [-] NOT found "termsrv.dll" version entry [%termsrv_dll_ver%] in file %rdpwrap_ini_check%^^!
-        if not "!rdpwrap_ini_update_github_%github_location%!" == "" (
-            set rdpwrap_ini_url=!rdpwrap_ini_update_github_%github_location%!
+    findstr /c:"[%termsrv_dll_ver%]" %rdpwrap_ini_check% >nul
+    if not errorlevel 1 (
+
+            echo [+] Found "termsrv.dll" version entry [%termsrv_dll_ver%] in file %rdpwrap_ini_check%.
+            echo [*] RDP Wrapper seems to be up-to-date and working...
+            :: call :reinstall
+            call :copynew
+            call :finish
+
+             
+    ) else (
+            echo [-] NOT found "termsrv.dll" version entry [%termsrv_dll_ver%] in file %rdpwrap_ini_check%^^!
+            
             call :update
-            goto :check_update
-        )
-        goto :finish
+
     )
+
+
 ) else (
+    :: if  %rdpwrap_ini_check% does not exsit, then exit 如果文件 %rdpwrap_ini_check% 不存在，则给出错误提示，并跳转至 :finish 结束程序。
     echo [-] File NOT found: %rdpwrap_ini_check%.
-    echo [*] Give up - Please check if Antivirus/Firewall blocks the file %rdpwrap_ini_check%^^!
-    goto :finish
+    echo [*] we will download the latest file %rdpwrap_new_ini%^^!
+    set rdpwrap_ini_check=%rdpwrap_new_ini%
+    copy /y NUL %rdpwrap_new_ini% >NUL
+    goto :check_update
 )
-goto :finish
-
-REM -----------------------------------------------------
-REM Install RDP Wrapper (exactly uninstall and reinstall)
-REM -----------------------------------------------------
-:install
-echo.
-echo [*] Uninstall and reinstall RDP Wrapper...
-echo.
-set rdpwrap_installed="1"
-%RDPWInst_exe% -u
-%RDPWInst_exe% -i -o
-call :setNLA
+:: Go to finish 最后跳转至 :finish 结束程序。
 goto :eof
 
-REM -------------------
-REM Restart RDP Wrapper
-REM -------------------
-:restart
-if %rdpwrap_installed%=="0" (
-    call :install
-)
-REM NOTE - normal copy of the file "rdpwrap_new.ini" to "rdpwrap.ini" will not work (file locked)
-REM        we need to stream the data from "rdpwrap_new.ini" to "rdpwrap.ini"
-if exist %rdpwrap_new_ini% (
-    echo [*] Start streaming %rdpwrap_new_ini% to %rdpwrap_ini%...
-    (
-        for /f "usebackq delims=" %%a in (
-            `findstr /n "^" %rdpwrap_new_ini%`
-        ) do (
-            if "!%%a!"=="" (
-                echo.
-            ) else (
-                echo !%%a!
-            )
-        )
-    )>%rdpwrap_ini%
-    echo [+] Update of %rdpwrap_ini% finished successfully.
-)
-echo.
-echo [*] Restart RDP Wrapper...
-echo.
-%RDPWInst_exe% -r
-call :setNLA
-goto :eof
 
-REM --------------------------------------------------------------------
-REM Download up-to-date (alternative) version of rdpwrap.ini from GitHub
-REM --------------------------------------------------------------------
+
+::
+:: --------------------------------------------------------------------
+:: Download up-to-date (alternative) version of rdpwrap.ini from GitHub
+:: --------------------------------------------------------------------
 :update
-set /a github_location=github_location+1
-echo.
+echo [*] check network connectivity...
+:netcheck
+ping -n 1 baidu.com>nul
+if errorlevel 1 (
+    goto waitnetwork
+) else (
+    goto download
+)
+
+:waitnetwork
+echo [.] Wait for network connection is available...
+ping 127.0.0.1 -n 11>nul
+set /a retry_network_check=retry_network_check+1
+:: wait for a maximum of 5 minutes
+if %retry_network_check% LSS 30 goto netcheck
+
+
+
+:download
 echo [*] Download latest version of rdpwrap.ini from GitHub...
 echo     -^> %rdpwrap_ini_url%
-for /f "tokens=* usebackq" %%a in (
-    `cscript //nologo "%~f0?.wsf" //job:fileDownload %rdpwrap_ini_url% %rdpwrap_new_ini%`
-) do (
-    set "download_status=%%a"
-)
-if "%download_status%"=="-1" (
+if not "%proxyserver%" == "" (
+curl --fail -s -x %proxyserver% -L %rdpwrap_ini_url% -o %rdpwrap_new_ini% 
+
+        )  else   (
+curl --fail -s -L %rdpwrap_ini_url% -o %rdpwrap_new_ini% 
+
+        )
+
+if %errorlevel%==0 (
     echo [+] Successfully download from GitHhub latest version to %rdpwrap_new_ini%.
     set rdpwrap_ini_check=%rdpwrap_new_ini%
-    call :restart
 ) else (
     echo [-] FAILED to download from GitHub latest version to %rdpwrap_new_ini%^^!
     echo [*] Please check you internet connection/firewall and try again^^!
 )
+
 goto :eof
 
-REM --------------------------------
-REM Set Network Level Authentication
-REM --------------------------------
-:setNLA
-echo [*] Set Network Level Authentication in the windows registry...
-reg.exe add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v SecurityLayer /t reg_dword /d 0x2 /f
-reg.exe add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v MinEncryptionLevel /t reg_dword /d 0x2 /f
+
+:: -------------------
+:: Restart RDP Wrapper
+:: -------------------
+:copynew
+echo.
+echo [*] copyfile...
+echo.
+
+
+@REM IF NOT EXIST "%rdpfolder%" (
+@REM     echo 1
+@REM     mkdir "%rdpfolder%"
+@REM )
+
+
+if exist %rdpwrap_new_ini% (
+    echo.
+    net stop termservice
+
+    echo [+] copy %rdpwrap_new_ini% to %rdpwrap_ini%...
+    xcopy /y %rdpwrap_new_ini% %rdpwrap_ini%
+::xcopy
+    net start termservice
+
+) else (
+    echo [x] ERROR - File %rdpwrap_new_ini% is missing ^^!
+)
+
+
 goto :eof
+
+:: -------------------
+:: Restart RDP Wrapper
+:: -------------------
+:reinstall
+echo.
+echo [*] Restart RDP Wrapper with new ini (uninstall and reinstall)...
+echo.
+%RDPWInst_exe% -u
+
+
+
+IF NOT EXIST "%rdpfolder%" (
+    echo 1
+    mkdir "%rdpfolder%"
+)
+
+
+if exist %rdpwrap_new_ini% (
+    echo.
+    echo [*] Use latest downloaded rdpwrap.ini from GitHub...
+    echo     -^> %rdpwrap_ini_url% 
+    echo       -^> %rdpwrap_new_ini%
+    echo         -^> %rdpwrap_ini%
+    echo [+] copy %rdpwrap_new_ini% to %rdpwrap_ini%...
+    xcopy /y %rdpwrap_new_ini% %rdpwrap_ini%
+::xcopy
+
+) else (
+    echo [x] ERROR - File %rdpwrap_new_ini% is missing ^^!
+)
+%RDPWInst_exe% -i -o
+goto :eof
+
+::
+:: -------------------
+:: Finish
+:: -------------------
 
 :finish
-echo.
-exit /b
-
-
---- Begin wsf script --- fileVersion/fileDownload --->
-<package>
-  <job id="fileVersion"><script language="VBScript">
-    set args = WScript.Arguments
-    Set fso = CreateObject("Scripting.FileSystemObject")
-    WScript.Echo fso.GetFileVersion(args(0))
-    Wscript.Quit
-  </script></job>
-  <job id="fileDownload"><script language="VBScript">
-    set args = WScript.Arguments
-    WScript.Echo SaveWebBinary(args(0), args(1))
-    Wscript.Quit
-    Function SaveWebBinary(strUrl, strFile) 'As Boolean
-        Const adTypeBinary = 1
-        Const adSaveCreateOverWrite = 2
-        Const ForWriting = 2
-        Dim web, varByteArray, strData, strBuffer, lngCounter, ado
-        On Error Resume Next
-        'Download the file with any available object
-        Err.Clear
-        Set web = Nothing
-        Set web = CreateObject("WinHttp.WinHttpRequest.5.1")
-        If web Is Nothing Then Set web = CreateObject("WinHttp.WinHttpRequest")
-        If web Is Nothing Then Set web = CreateObject("MSXML2.ServerXMLHTTP")
-        If web Is Nothing Then Set web = CreateObject("Microsoft.XMLHTTP")
-        web.Open "GET", strURL, False
-        web.Send
-        If Err.Number <> 0 Then
-            SaveWebBinary = False
-            Set web = Nothing
-            Exit Function
-        End If
-        If web.Status <> "200" Then
-            SaveWebBinary = False
-            Set web = Nothing
-            Exit Function
-        End If
-        varByteArray = web.ResponseBody
-        Set web = Nothing
-        'Now save the file with any available method
-        On Error Resume Next
-        Set ado = Nothing
-        Set ado = CreateObject("ADODB.Stream")
-        If ado Is Nothing Then
-            Set fs = CreateObject("Scripting.FileSystemObject")
-            Set ts = fs.OpenTextFile(strFile, ForWriting, True)
-            strData = ""
-            strBuffer = ""
-            For lngCounter = 0 to UBound(varByteArray)
-                ts.Write Chr(255 And Ascb(Midb(varByteArray,lngCounter + 1, 1)))
-            Next
-            ts.Close
-        Else
-            ado.Type = adTypeBinary
-            ado.Open
-            ado.Write varByteArray
-            ado.SaveToFile strFile, adSaveCreateOverWrite
-            ado.Close
-        End If
-        SaveWebBinary = True
-    End Function
-  </script></job>
-</package>
+echo "all done"
+pause
+exit 
